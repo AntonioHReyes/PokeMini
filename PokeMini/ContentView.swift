ab851cea3554918089efea0719be1cbb07997887
antoniohuertareyes <anhr9728@gmail.com>
2026-08-06 14:05:09 -0600

    Initial Commit

---
+++ PokeMini/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeMini
+//
+//  Created by Antonio Huerta Reyes on 06/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeMini/PokeMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeMiniApp.swift
+//  PokeMini
+//
+//  Created by Antonio Huerta Reyes on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PokeMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
